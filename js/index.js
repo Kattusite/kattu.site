a1ecd91b751f34dd6434c1a4ec7f6331fbb1d7cf
kattusite <kattusite@gmail.com>
2019-06-05 02:10:51 -0400

    param tweaks

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const interval  = 750;
 const maxSparks = 80;
-const avgDur    = 6;
-const varDur    = 2;
+const avgDur    = 4;
+const varDur    = avgDur * 0.25;
 const height    = 80;
 const width     = 4;
 
