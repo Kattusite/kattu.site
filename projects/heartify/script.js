af7e85fab92f378a3e102ce39d2e4ffe1bd9fede
kattusite <kattusite@gmail.com>
2019-07-18 01:24:14 -0400

    style changes, starter project page, fixing links

--- projects/heartify/script.js
+++ projects/heartify/script.js
@@ -1,3 +1,5 @@
+let heart = "%E2%9D%A4";
+
 function submit() {
   let textarea = document.getElementById('fetcharea');
   let text = textarea.value;
@@ -5,13 +7,16 @@ function submit() {
   // increment mutation id
   let mut_re = /client_mutation_id%22%3A%22(\d+)%/
   let seq = text.match(mut_re);
+
+  if (!seq || seq.length < 2) return;
+
   let seqnum = parseInt(seq[1]) + 1
   text = text.replace(mut_re, "client_mutation_id%22%3A%22" + seqnum + "%");
 
   // change emoji
   text = text.replace(
     /reaction%22%3A%22(%[0-9A-F][0-9A-F])+%22%7D%7D/,
-    "reaction%22%3A%22%E2%9D%A4%22%7D%7D"
+    "reaction%22%3A%22" + heart + "%22%7D%7D"
   )
 
   textarea.value = text;
